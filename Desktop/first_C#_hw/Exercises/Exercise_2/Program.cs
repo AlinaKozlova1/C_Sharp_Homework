a39f9809b5f4d2ad169333fc21381cac8067bfc8
Alina <alinakozlova908@gmail.com>
2022-06-30 19:11:34 +0300

    did the first exercise

---
+++ Desktop/first_C#_hw/Exercises/Exercise_2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("The sum of your numbers is equal to ");
+int sum = a + b;
+Console.WriteLine(sum);
