2c068bf4ee24ec20b0fe3d53e410c75f73d10ef7
Alina <alinakozlova908@gmail.com>
2022-07-02 12:39:14 +0300

    did the fourth exercise

---
+++ Desktop/first_C#_hw/Exercises/Exercise_8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2 != 0)
+{
+    number = number -1;
+}
+while (number>1)
+{
+    Console.Write(number);
+    if (number!=2)
+    {
+        Console.Write(",");
+    }
+    number = number - 2;
+}
