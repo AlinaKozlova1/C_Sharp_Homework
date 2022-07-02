0b4d47bd0113b440e1b9b453a229489765f38235
Alina <alinakozlova908@gmail.com>
2022-07-02 12:01:24 +0300

    did the third exercise

---
+++ Desktop/first_C#_hw/Exercises/Exercise_6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter the number. This program'll determine is your number even or not: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2 == 0)
+{
+    Console.WriteLine(number +  " - Yes, it's even");
+}
+else
+{
+    Console.WriteLine(number + " - Nope, it's not even");
+}
