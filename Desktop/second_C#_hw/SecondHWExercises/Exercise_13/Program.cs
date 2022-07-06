5a2584f970c60be791550047f566c14aea64493b
Alina <alinakozlova908@gmail.com>
2022-07-06 14:25:26 +0300

    did the second exercise

---
+++ Desktop/second_C#_hw/SecondHWExercises/Exercise_13/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("---------------------");
+Console.Write("Please, enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<100)
+{
+    Console.WriteLine("There is no the third digit~");
+}
+else
+{
+    void FindTheThirdDigit(int ThreeDigitNumber)
+{
+        ThreeDigitNumber = ThreeDigitNumber%10;
+        Console.WriteLine("The third digit of your numer is " + ThreeDigitNumber);
+}
+
+    void CutNumber (int Number)
+{
+        while (Number>999)
+{
+            int balace = Number%10;
+            Number = (Number - balace)/10;
+}  
+        FindTheThirdDigit(Number);
+}
+
+    CutNumber(number);
+}
+
+
