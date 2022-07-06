dab9eddeebb36dc422982fd2bca533364b217c04
Alina <alinakozlova908@gmail.com>
2022-07-06 16:11:24 +0300

    change the folder for the first exercise

---
+++ Desktop/second_C#_hw/SecondHWExercises/Exercise__10/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("---------------------");
+Console.Write("Please, enter three-digit number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (999<numberA)                     //проверяем является ли число 3х-значным
+{
+    Console.WriteLine("Your number is too large");
+}
+if (numberA<100)
+{
+    Console.WriteLine("Your number is too small");
+}
+else
+{
+    int balance = numberA%100;                                 //отсекаем остаток
+    int SecondNumber = balance/10;                             //находим среднюю цифру
+    Console.WriteLine("The second digit is " + SecondNumber);
+}
