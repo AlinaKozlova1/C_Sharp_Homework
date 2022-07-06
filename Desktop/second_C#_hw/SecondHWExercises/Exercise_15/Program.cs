57d2245aeb5de1f97dbb620247737a7cfbf264cf
Alina <alinakozlova908@gmail.com>
2022-07-06 16:06:00 +0300

    did the third exercise

---
+++ Desktop/second_C#_hw/SecondHWExercises/Exercise_15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("---------------------");
+Console.Write("Please, enter a weekday number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA<1|numberA>7)
+{
+    Console.WriteLine("Enter the number from 1 to 7");
+} 
+else
+{
+    if (numberA>5)
+    {
+        Console.Write(numberA + " - day off");
+    }
+    else
+    {
+        Console.Write(numberA + " - working day");
+    }
+}
