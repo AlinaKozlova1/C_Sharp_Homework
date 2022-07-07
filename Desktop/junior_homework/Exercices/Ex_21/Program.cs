6ff1b31794b67791423316052f4553c109f7eb86
Alina <alinakozlova908@gmail.com>
2022-07-07 18:19:17 +0300

    add the second exercise

---
+++ Desktop/junior_homework/Exercices/Ex_21/Program.cs
@@ -0,0 +1,7 @@
+﻿double distance(double xb, double xa, double yb, double ya, double zb, double za)
+{
+    double S = Math.Sqrt(Math.Pow(xb-xa, 2) + Math.Pow(yb-ya, 2) + Math.Pow(zb-za, 2));
+    Console.WriteLine("Distance = " + S);
+    return S;
+}
+distance(15,13,6,3,8,2);
