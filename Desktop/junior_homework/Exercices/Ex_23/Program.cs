f4ccddacd6984ec8cb2ea7c82c89c1d8000c9051
Alina <alinakozlova908@gmail.com>
2022-07-07 19:05:58 +0300

    add the third exercise

---
+++ Desktop/junior_homework/Exercices/Ex_23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int[] Cube( int number)
+{
+    int[] a = new int [number];
+    for (int i=1; i<=number; i++)
+    {
+        a[i-1] = i*i*i;
+        Console.WriteLine(a[i-1]);
+    }
+    return a;
+}
+
+Cube(num);
