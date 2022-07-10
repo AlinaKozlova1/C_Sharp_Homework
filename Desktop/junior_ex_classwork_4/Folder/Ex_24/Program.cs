2079cf648bbae2972b3ece0de354a6f25965206d
Alina <alinakozlova908@gmail.com>
2022-07-10 20:01:43 +0300

    add an exercise about amount of numbers

---
+++ Desktop/junior_ex_classwork_4/Folder/Ex_24/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum (int Num)
+{
+    int i = 0;
+    int amount = 0;
+    while (i<=Num)
+    {
+        amount = amount+i;
+        i++;
+    }
+    Console.WriteLine(amount);
+    return amount;
+}
+
+sum(number);
