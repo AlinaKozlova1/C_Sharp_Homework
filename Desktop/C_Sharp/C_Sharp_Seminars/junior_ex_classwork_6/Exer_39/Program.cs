bdca611ee68495c4f2691d6cee4daf1b883303ae
Alina <alinakozlova908@gmail.com>
2022-07-20 12:38:27 +0300

    add the 39th file

---
+++ Desktop/C_Sharp/C_Sharp_Seminars/junior_ex_classwork_6/Exer_39/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[5]{1, 2, 3, 4, 5};
+
+void arrayReversal (int[] arr)
+{
+    for (int i = 0; i < arr.Length/2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - i - 1];
+        arr[arr.Length - i - 1] = temp;
+    }
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+
+arrayReversal(array);
